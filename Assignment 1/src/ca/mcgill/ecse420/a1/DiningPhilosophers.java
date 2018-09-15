d85842edd7cd81e9f40e6800f83abbd7146d6ed1
Sam Cleland <edward.cleland@mail.mcgill.ca>
2018-09-15 06:41:16 -0400

    Update dining philosophers, works

--- Assignment 1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ Assignment 1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -2,24 +2,30 @@ package ca.mcgill.ecse420.a1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DiningPhilosophers {
 
-	private static Chopstick[] chopsticks;
-	private static int numberOfPhilosophers;
+	private static Lock[] chopsticks;
+	private static int numberOfPhilosophers = 5;
 	private static final Logger logger = Logger.getLogger(DiningPhilosophers.class.getName());
 
 	public static void main(String[] args) {
 
-		numberOfPhilosophers = 5;
 		Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-		chopsticks = new Chopstick[numberOfPhilosophers];
+		chopsticks = new Lock[numberOfPhilosophers];
 		ExecutorService executor = Executors.newFixedThreadPool(numberOfPhilosophers);
+
+		for (int i = 0; i < numberOfPhilosophers; i++) {
+			chopsticks[i] = new ReentrantLock();
+		}
+
 		for (int i = 0; i < numberOfPhilosophers; i++) {
 			philosophers[i] = new Philosopher(i);
-			chopsticks[i] = new Chopstick();
 		}
 
 		for (Philosopher philosopher : philosophers) {
@@ -29,48 +35,47 @@ public class DiningPhilosophers {
 
 	public static class Philosopher implements Runnable {
 
-		private int number;
+		private ReentrantLock leftChopstick;
+		private ReentrantLock rightChopstick;
+		private String philosopherName;
 
 		public Philosopher(int number) {
-			this.number = number;
+			this.philosopherName = "Philosopher " + number;
+			this.leftChopstick = (ReentrantLock) chopsticks[number - 1 < 0 ? numberOfPhilosophers - 1 : number - 1];
+			this.rightChopstick = (ReentrantLock) chopsticks[number];
 		}
 
 		@Override
 		public void run() {
 			while (true) {
-				Chopstick leftChopstick = chopsticks[(number - 1) % numberOfPhilosophers];
-				Chopstick rightChopstick = chopsticks[(number + 1) % numberOfPhilosophers];
 				try {
-					leftChopstick.wait();
-					synchronized (leftChopstick) {
-						rightChopstick.wait();
-						synchronized (rightChopstick) {
-
-						}
+					System.out.println(philosopherName + " is thinking...");
+					leftChopstick.lock();
+					if (rightChopstick.tryLock(1, TimeUnit.SECONDS)) {
+						System.out.println(philosopherName + " is eating...");
+						sleepFor(3000);
+					} else {
+						System.out.println(philosopherName + " couldn't acquire both forks...releasing....");
 					}
 				} catch (InterruptedException ix) {
 					logger.log(Level.SEVERE, ix.getMessage());
+				} finally {
+					leftChopstick.unlock();
+					if (rightChopstick.isHeldByCurrentThread()) {
+						rightChopstick.unlock();
+					}
+
+					sleepFor(2000);
 				}
 			}
 		}
+
+		private void sleepFor(int duration) {
+			try {
+				Thread.sleep(duration);
+			} catch (InterruptedException ix) {
+				logger.log(Level.SEVERE, ix.getMessage());
+			}
+		}
 	}
-
-	public static class Chopstick {
-
-		private boolean isFree;
-
-		public Chopstick() {
-			this.isFree = true;
-		}
-
-		public boolean isFree() {
-			return isFree;
-		}
-
-		public void setFree(boolean isFree) {
-			this.isFree = isFree;
-		}
-
-	}
-
 }
