b6802b2a6dc3b1920a4544aa7929c5f9276a32c6
Sam Cleland <edward.cleland@mail.mcgill.ca>
2018-11-22 17:18:38 -0500

    Scanner for question 3

--- Assignment 3/src/ca/mcgill/ecse420/a3/q3/TestBoundedArrayQueue.java
+++ Assignment 3/src/ca/mcgill/ecse420/a3/q3/TestBoundedArrayQueue.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse420.a3.q3;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
@@ -7,11 +8,21 @@ import java.util.stream.IntStream;
 public class TestBoundedArrayQueue {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter 1 for Lock free, anything else for normal");
 		ExecutorService service = Executors.newCachedThreadPool();
-		LockFreeBoundedArrayQueue.populateSlots();
-		IntStream.range(0, 10).forEach(i -> service.execute(new LockFreeBoundedArrayQueue()));
-		//BoundedArrayQueue.populateSlots();
-		//IntStream.range(0, 50).forEach(i -> service.execute(new BoundedArrayQueue()));
+		String input = userInput.nextLine();
+		userInput.close();
+		if(input.trim().equals("1")) {
+			System.out.println("Lock Free Bounded Queue");
+			LockFreeBoundedArrayQueue.populateSlots();
+			IntStream.range(0, 50).forEach(i -> service.execute(new LockFreeBoundedArrayQueue()));
+		} else {
+			System.out.println("Normal Bounded Queue");
+			BoundedArrayQueue.populateSlots();
+			IntStream.range(0, 50).forEach(i -> service.execute(new BoundedArrayQueue()));
+		}
+
 		service.shutdown();
 	}
 
